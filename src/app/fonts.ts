1db207abc623e458c76c10a18489d0460cf7c487
Yuri <yuripinage@gmail.com>
2025-09-03 18:35:12 +0100

    apply custom fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from 'next/font/local';
+
+export const w95fa = localFont({
+	src: '../public/fonts/W95FA.otf',
+	variable: '--font-w95fa',
+});
+
+export const firsNeue = localFont({
+	src: '../public/fonts/TTFirsNeue.ttf',
+	variable: '--font-firs',
+});
